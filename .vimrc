25ac3eabd5c21fbd14f22fe7847990ddf47e87ac
Joshua Songy <joshua.songy@gmail.com>
2014-11-13 16:17:52 -0500

    Converted configuration to GNU stow format.

---
+++ .vimrc
@@ -0,0 +1,5 @@
+" ######################
+" # Redirect to Vim RC #
+" ######################
+
+source ~/.vim/common/rc.vim
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "$DIR"
-git pull origin master
-vim -u "~/.vim/vundle.vim" -c "PluginInstall!" -c ":q" -c ":q"
